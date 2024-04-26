1a90900ec2183c8edb843763da2be37175ebf8ed
Steve <estebanhirzfeld@gmail.com>
2024-04-26 17:25:55 -0300

    feat: java coding

---
+++ Java/Ejemplo.java
@@ -0,0 +1,13 @@
+public class Ejemplo {
+    public static int suma(String args[]){
+        return 52;
+    }
+
+    public static void main(String args[]){
+        int variable = 11;
+        int valor = suma(args);
+        System.out.print("Hello World " + variable);
+        System.out.println("Hello World " + valor);
+        System.out.print("Hello World " + variable);
+    }
+}
