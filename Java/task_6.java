2910759da564c17c218205be245562a2a8d5b30d
Steve <estebanhirzfeld@gmail.com>
2024-05-03 17:35:16 -0300

    clase_8

--- Java/task_6.java
+++ Java/task_6.java
@@ -5,17 +5,34 @@ import java.util.Scanner;
 public class task_6 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String line = "* * * * * * * *";
-        String space = "*             *";
+        String line = "";
+        // String line = "* * * * * * * *";
+        String space = "*";
         
         
         System.out.println("Ingrese cant de columnas");
+        int columnas = sc.nextInt();
+        System.out.println("Ingrese cant de filas");
         int filas = sc.nextInt();
-        System.out.println(line);
-        for (int i = 0; i < filas; i++) {
-            System.out.println(space);
+
+        // Columnas
+        for (int i = 0; i < columnas; i++) {
+            line = line + "* ";
         }   
-        System.out.println(line);
+        
+        // Filas
+        for (int i = 0; i < columnas*2-3; i++) {
+            space = space + " ";
+        }   
+        // Square
+        System.out.println(line.strip());
+
+        for (int i = 0; i < filas-2; i++) {
+            System.out.println(space + "*");
+        }   
+
+        System.out.println(line.strip());
+
         System.out.println("line: " + line.length());
         System.out.println("space: " + space.length());
     }
