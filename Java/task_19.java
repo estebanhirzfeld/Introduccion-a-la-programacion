27e06f0b97abb260ed399deb5c269d862a040ea1
Steve <estebanhirzfeld@gmail.com>
2024-05-17 17:33:54 -0300

    clase_11

---
+++ Java/task_19.java
@@ -0,0 +1,14 @@
+
+
+public class task_19 {
+
+    public static void main(String[] args) {
+        Integer numbers[] = new Integer[2];
+        String people[] = {"Steve", "Tony"};
+
+        numbers[1] = 11;
+        System.out.println(numbers[1]);
+        System.out.println(people[0]);
+
+    }
+}
