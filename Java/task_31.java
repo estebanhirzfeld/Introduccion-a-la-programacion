84da859f0bd33019862772433da9bc087b7ba390
Steve <estebanhirzfeld@gmail.com>
2024-06-04 15:46:37 -0300

    clase_14

---
+++ Java/task_31.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class task_31 {
+
+    public static Integer suma(Integer x, Integer y) {
+        return x + y;
+    }
+
+    public static Integer resta(Integer x, Integer y) {
+        return x - y;
+    }
+
+    public static Integer multiplicacion(Integer x, Integer y) {
+        return x * y;
+    }
+
+    public static Integer division(Integer x, Integer y) {
+        return x / y;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
